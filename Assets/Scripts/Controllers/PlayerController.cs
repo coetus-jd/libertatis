69861818299e59c694202e8f6e130b4b898345f5
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-04-30 19:07:50 -0300

    cria scene para cadastro de um player

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PirateCave.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        /// <summary>
+        /// A velocidade com que o player irá se mover
+        /// </summary>
+        [SerializeField]
+        private float velocity;
+
+        void Update()
+        {
+            move();
+        }
+
+        private void move()
+        {
+            float currentXPosition = gameObject.transform.position.x;
+
+            gameObject.transform.Translate(new Vector3(
+                currentXPosition + (1 * velocity),
+                gameObject.transform.position.y,
+                gameObject.transform.position.z)
+            );
+        }
+    }
+}
