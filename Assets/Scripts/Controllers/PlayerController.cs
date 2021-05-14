f10b24ea30dc798947d493ecd1fa272e0db3b61f
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-05-13 22:17:21 -0300

    adiciona animação de andar

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,25 +7,73 @@ namespace PirateCave.Controllers
     public class PlayerController : MonoBehaviour
     {
         /// <summary>
+        /// A altura máxima da tela
+        /// </summary>
+        private const float maxYPosition = 1.35f;
+
+        /// <summary>
+        /// A altura mínima da tela
+        /// </summary>
+        private const float minYPosition = -1.16f;
+
+        /// <summary>
+        /// A largura máxima da tela
+        /// </summary>
+        private const float maxXPosition = 2.61f;
+
+        /// <summary>
+        /// A largura mínima da tela
+        /// </summary>
+        private const float minXPosition = -2.64f;
+
+        /// <summary>
         /// A velocidade com que o player irá se mover
         /// </summary>
         [SerializeField]
-        private float velocity;
+        private float velocity = 8.0f;
 
-        void Update()
+        private Animator animator;
+
+        void Start()
         {
-            move();
+            animator = GetComponent<Animator>();
         }
+
+        void Update() => move();
 
         private void move()
         {
-            float currentXPosition = gameObject.transform.position.x;
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
 
-            gameObject.transform.Translate(new Vector3(
-                currentXPosition + (1 * velocity),
-                gameObject.transform.position.y,
-                gameObject.transform.position.z)
-            );
+            handleAnimation(verticalInput, horizontalInput);
+
+            transform.Translate(((Vector3.right * velocity) * horizontalInput) * Time.deltaTime);
+            transform.Translate(((Vector3.up * velocity) * verticalInput) * Time.deltaTime);
+
+            restrictUserMovement();
+        }
+
+        private void handleAnimation(float verticalValue, float horizontalValue)
+        {
+            animator.SetBool("walking", (horizontalValue != 0.0f));
+        }
+
+        private void restrictUserMovement()
+        {
+            // Vertical
+            if (transform.position.y > maxYPosition)
+                transform.position = new Vector3(transform.position.x, maxYPosition, 0);
+
+            if (transform.position.y < minYPosition)
+                transform.position = new Vector3(transform.position.x, minYPosition, 0);
+            
+            // Horizontal 
+            if (transform.position.x > maxXPosition)
+                transform.position = new Vector3(maxXPosition, transform.position.y, 0);
+            
+            if (transform.position.x < minXPosition)
+                transform.position = new Vector3(minXPosition, transform.position.y, 0);
         }
     }
 }
