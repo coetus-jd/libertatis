8a668a0012c23fb68d852c7ce60f41ef099d6863
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-05-26 19:55:34 -0300

    adiciona o tileset do fort

---
+++ Assets/Scripts/Controllers/Colliders/TrunkController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using PirateCave.Enums;
+
+namespace PirateCave.Controllers.Colliders
+{
+    public class TrunkController : MonoBehaviour
+    {
+        private PhaseController phaseController;
+        
+        void Awake()
+        {
+            phaseController = GameObject.FindGameObjectWithTag(Tags.PhaseController)
+                .GetComponent<PhaseController>();
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!col.gameObject.CompareTag(Tags.Player))
+                return;
+        }
+    }
+}
---
+++ Assets/Scripts/Enums/Tags.cs
@@ -0,0 +1,18 @@
+namespace PirateCave.Enums
+{
+    /// <summary>
+    /// Todas as tags utilizadas na Unity
+    /// </summary>
+    public static class Tags
+    {
+        /// <summary>
+        /// Tag utilizado no player
+        /// </summary>
+        public static string Player = "player";
+
+        /// <summary>
+        /// Tag utilizado no objeto de phase controller
+        /// </summary>
+        public static string PhaseController = "phaseController";
+    }
+}
