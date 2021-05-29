43b9b789fdc86ba96fa4809af9c61257dd03d89d
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-05-29 14:29:27 -0300

    alterações visuais na scene home

---
+++ Assets/Scripts/Utilities/ChangeTextWithPlayerNick.cs
@@ -0,0 +1,22 @@
+﻿using PirateCave.Controllers;
+using TMPro;
+using UnityEngine;
+
+namespace PirateCave.Utilities
+{
+    public class ChangeTextWithPlayerNick : MonoBehaviour
+    {
+        /// <summary>
+        /// Componente de texto que irá mostrar o nick do jogador
+        /// </summary>
+        private TextMeshProUGUI uiPlayerNick;
+
+        void Awake()
+        {
+            uiPlayerNick = gameObject.GetComponent<TextMeshProUGUI>();
+
+            if (uiPlayerNick)
+                uiPlayerNick.text = GameController.loggedPlayer?.nick;
+        }
+    }
+}
