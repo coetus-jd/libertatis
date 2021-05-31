b178e8815b2bb10ae4c96bc05d11e3f15257c674
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-05-31 14:13:03 -0300

    ajustes na lógica para abrir o baú

--- Assets/Scripts/Controllers/Colliders/TrunkController.cs
+++ Assets/Scripts/Controllers/Colliders/TrunkController.cs
@@ -13,17 +13,37 @@ namespace PirateCave.Controllers.Colliders
         [SerializeField]
         private AudioClip catchAudio;
 
+        /// <summary>
+        /// Boolean que verifica se o jogador está dentro do collider do manuscrito
+        /// </summary>
+        private bool playerIsIn;
+
         void Awake()
         {
             phaseController = GameObject.FindGameObjectWithTag(Tags.PhaseController)
                 .GetComponent<PhaseController>();
         }
 
-        void OnTriggerStay2D(Collider2D col)
+        void Update()
         {
-            if (!col.gameObject.CompareTag(Tags.Player))
-                return;
+            if (playerIsIn)
+                openTrunk();
+        }
 
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag(Tags.Player))
+                playerIsIn = true;
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag(Tags.Player))
+                playerIsIn = false;
+        }
+
+        private void openTrunk()
+        {
             if (Input.GetKeyDown(KeyCode.X))
             {
                 AudioSource.PlayClipAtPoint(catchAudio, gameObject.transform.position);
