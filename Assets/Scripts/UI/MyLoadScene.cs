c6443afddb0b5403ca7f4b6aca34c14aa9283fbd
Jonatas Freire <jonatas.freire@devitecnologia.com.br>
2021-06-01 00:24:17 -0300

    Add Blink to tesxt

---
+++ Assets/Scripts/UI/MyLoadScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyLoadScene : MonoBehaviour
+{
+    public string SceneName;
+    public bool m_UseTouch;
+
+    public AudioSource m_Clip;
+
+    public void Update()
+    {
+        if (m_UseTouch)
+        {
+            if (Input.anyKeyDown)
+            {
+                LoadSceneName(SceneName);
+            }
+        }
+    }
+
+    //https://docs.unity3d.com/ScriptReference/AudioSource.html
+    //https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadScene.html
+    public void LoadSceneName(string sceneName)
+    {
+        if (m_Clip)
+        {
+            m_Clip = GetComponent<AudioSource>();
+            m_Clip.Play(0);
+        }
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
