e1544d009b4377f2a4f87f6f9cca50c767e41c3d
WaterWhirl <leonardo-zende@hotmail.com>
2021-06-08 17:09:40 -0300

    Bau pontuação

--- Assets/Scripts/Controllers/Colliders/TrunkController.cs
+++ Assets/Scripts/Controllers/Colliders/TrunkController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using PirateCave.Enums;
+using UnityEngine.UI;
 
 namespace PirateCave.Controllers.Colliders
 {
@@ -17,6 +18,13 @@ namespace PirateCave.Controllers.Colliders
         private Animator animator;
 
         /// <summary>
+        /// Pontuação ganha por cada tesouro e atualização de pontos na tela
+        /// </summary>
+        private float treasureScore;
+        public Text txtScore;
+        bool openTreasure = false;
+
+        /// <summary>
         /// Boolean que verifica se o jogador está dentro do collider do manuscrito
         /// </summary>
         private bool playerIsIn;
@@ -32,11 +40,53 @@ namespace PirateCave.Controllers.Colliders
             if (playerIsIn)
             {
                 // openTrunk();
-                if (Input.GetKeyDown(KeyCode.X))
+                if (Input.GetKeyDown(KeyCode.X) && openTreasure == false)
+                {
                     animator.SetBool("open", true);
+                    getTreasure();
+                    PlayerPrefs.SetFloat("treasureScore", treasureScore);
+                    openTreasure = true;
+                }
+                    
                 
             }
         }
+
+        private void getTreasure()
+        {
+            float randomNumber = Random.Range(1f, 50f);
+            
+
+                if (randomNumber <= 20)
+                {
+                    treasureScore = treasureScore + 1;  //Calice
+                }
+                else if (randomNumber <= 30)
+                {
+                    treasureScore = treasureScore + 2;  //Dobrão
+                }
+                else if (randomNumber <= 38)
+                {
+                    treasureScore = treasureScore + 3;  //Anel
+                }
+                else if (randomNumber <= 44)
+                {
+                    treasureScore = treasureScore + 4;  //Diamante vermelho
+                }
+                else if (randomNumber <= 48)
+                {
+                    treasureScore = treasureScore + 5;  //Diamante Azul
+                }
+                else
+                {
+                    treasureScore = treasureScore + 6;  //Coroa
+                }
+
+                txtScore.text = "" + treasureScore;
+            
+
+        }
+
 
         void OnTriggerEnter2D(Collider2D col)
         {
