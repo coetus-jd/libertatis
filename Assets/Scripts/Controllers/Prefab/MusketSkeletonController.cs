a3c6f5a5f1c229a0ba5603841525a1816d98aea4
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-06-11 08:16:12 -0300

    animação do esqueleto atirando + lógica

--- Assets/Scripts/Controllers/Prefab/MusketSkeletonController.cs
+++ Assets/Scripts/Controllers/Prefab/MusketSkeletonController.cs
@@ -7,30 +7,65 @@ namespace PirateCave.Controllers.Prefab
         /// <summary>
         /// A posição da qual a bala irá sair
         /// </summary>
+        [SerializeField]
         private Transform musketBulletShootPosition;
 
         /// <summary>
         /// Prefab da bala que o esqueleto irá disparar
         /// </summary>
+        [SerializeField]
         private GameObject bulletPrefab;
 
         /// <summary>
         /// Animator com todas as animações do corsário
         /// </summary>
+        [SerializeField]
         private Animator animator;
 
+        /// <summary>
+        /// Indica se o esqueleto está atirando ou não
+        /// </summary>
         private bool isShooting;
+
+        /// <summary>
+        /// Indica se o esqueleto está recarregando ou não
+        /// </summary>
+        private bool isRecharging;
+
+        /// <summary>
+        /// Quantidade de tiros que o esqueleto dará até recarregar
+        /// </summary>
+        private const int shootQuantityToRecharge = 3;
+
+        /// <summary>
+        /// Quantidade de tiros que o esqueleto já deu
+        /// </summary>
+        [SerializeField]
+        private int shootQuantity;
 
         void Update()
         {
-            if (!isShooting)
+            if (!isShooting && !isRecharging)
                 attackPlayer();
+        }
+
+        private void recharge()
+        {
+            animator.SetBool("recharge", true);
+            isRecharging = true;
         }
 
         private void attackPlayer()
         {
+            if (shootQuantity > shootQuantityToRecharge)
+            {
+                recharge();
+                return;
+            }
+
             animator.SetBool("shooting", true);
             isShooting = true;
+            shootQuantity++;
         }
 
         /// <summary>
@@ -49,6 +84,16 @@ namespace PirateCave.Controllers.Prefab
         {
             animator.SetBool("shooting", false);
             isShooting = false;
+        }
+
+        /// <summary>
+        /// Essa função será chamada no final da animação de recarregar
+        /// </summary>
+        private void stopRecharge()
+        {
+            animator.SetBool("recharge", false);
+            shootQuantity = 0;
+            isRecharging = false;
         }
     }
 }
