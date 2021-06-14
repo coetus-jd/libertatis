7a7e95c212dca24a79acda3a9cefb87b61a37bb0
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-06-14 20:17:48 -0300

    adiciona tiles organizados

--- Assets/Scripts/Controllers/Colliders/HookController.cs
+++ Assets/Scripts/Controllers/Colliders/HookController.cs
@@ -15,20 +15,20 @@ namespace PirateCave.Controllers.Colliders
         [SerializeField]
         private Rigidbody2D rigidBody;
 
-        private HingeJoint2D ropeEffect;
+        private DistanceJoint2D ropeEffect;
 
         void Start()
         {
-            ropeEffect = GetComponent<HingeJoint2D>();
+            ropeEffect = player.GetComponent<DistanceJoint2D>();
         }
 
         void OnTriggerEnter2D(Collider2D col)
         {
             if (!col.gameObject.CompareTag(Tags.PlayerHook))
                 return;
-            Debug.Log("Entrou");
 
-            ropeEffect.connectedBody = player.GetComponent<Rigidbody2D>();
+            ropeEffect.enabled = true;
+            // ropeEffect.connectedBody = GetComponent<Rigidbody2D>();
             
             col.gameObject
                 .GetComponentInParent<PlayerController>()
