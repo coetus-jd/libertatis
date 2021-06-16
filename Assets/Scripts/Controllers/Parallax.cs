d709d78b85afc0ffbe93deab2255042526577480
João Frederico Kraft <fredcuppi@gmail.com>
2021-06-16 18:01:29 -0300

    Revert "a"
    This reverts commit aa04faefeee5e9b9adc2352733690fef447fc7c4.

--- Assets/Scripts/Controllers/Parallax.cs
+++ Assets/Scripts/Controllers/Parallax.cs
@@ -24,7 +24,7 @@ public class Parallax : MonoBehaviour
         float temp = (camPlayer.transform.position.x * (1 - speedParallax));
         float dist = (camPlayer.transform.position.x * speedParallax);
 
-        transform.position = new Vector3(startPos + dist, 0f, 0f);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
 
         if(temp > startPos + lenght)
         {
